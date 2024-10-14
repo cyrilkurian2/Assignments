095c45974b905e10d00986f56d5aa32bc3ee3718
cyrilkurian2 <cyril.kurian@geekywolf.local>
2024-10-14 09:26:17 +0530

    added all routes

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,31 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import {TableCreationComponent} from  './table-creation/table-creation.component';
-import {CreateLinkToGoogleComponent} from './create-link-to-google/create-link-to-google.component';
-import {CreateListWithColorComponent} from './create-list-with-color/create-list-with-color.component';
-import {CreateNestedListComponent} from './create-nested-list/create-nested-list.component';
-import {CreateAlertMeButtonComponent} from './create-alert-me-button/create-alert-me-button.component';
-import {CreateLeapYearComponent} from './create-leap-year/create-leap-year.component';
-import {CreateShoppingListComponent} from './create-shopping-list/create-shopping-list.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
-    TableCreationComponent,
-    CreateLinkToGoogleComponent,
-    CreateListWithColorComponent,
-    CreateNestedListComponent,
-    CreateAlertMeButtonComponent,
-    CreateLeapYearComponent,
-    CreateShoppingListComponent
-  ],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-
-
 export class AppComponent {
-  title = 'first-app';
+  title = 'routing-app';
 }
--- first-app/src/app/app.routes.ts
+++ first-app/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CreateAlertMeButtonComponent } from './create-alert-me-button/create-alert-me-button.component';
+import { CreateLinkToGoogleComponent } from './create-link-to-google/create-link-to-google.component';
+import { CreateListWithColorComponent} from './create-list-with-color/create-list-with-color.component';
+import { CreateNestedListComponent} from './create-nested-list/create-nested-list.component';
+import { TableCreationComponent} from './table-creation/table-creation.component';
+import { CreateLeapYearComponent} from './create-leap-year/create-leap-year.component';
+import { CreateShoppingListComponent} from './create-shopping-list/create-shopping-list.component';
+
+export const routes: Routes = [
+
+    { path: 'alert-box-button', component: CreateAlertMeButtonComponent},
+    { path: 'link-to-google', component: CreateLinkToGoogleComponent},
+    { path: 'list-with-color', component: CreateListWithColorComponent},
+    { path: 'nested-list', component: CreateNestedListComponent},
+    { path: 'table-creation', component: TableCreationComponent},
+    { path: 'leap-year', component:CreateLeapYearComponent},
+    { path: 'shopping-list', component: CreateShoppingListComponent},
+  ];
